d5d17614790e0f0d4fd6d5ac18fdd0b74ba12afb
Theophile-Kango <fadhili.kango@gmail.com>
2020-08-06 08:05:18 +0200

    Add containers folder to the project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MoviesList from './../container/MoviesList';
+import './../App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+     <MoviesList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/container/MoviesList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MoviesList = () => {
+    return(
+        <h1>Hello World!</h1>
+    )
+}
+
+export default MoviesList;
